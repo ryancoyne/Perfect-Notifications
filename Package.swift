8a596be1f929c847bfbb009eaf5199d899d8d976
Ryan <ryan@buckettechnologies.com>
2019-01-06 20:30:01 -0500

    Updating the package file

--- Package.swift
+++ Package.swift
@@ -7,11 +7,11 @@ let package = Package(
 		.library(name: "PerfectNotifications", targets: ["PerfectNotifications"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/PerfectlySoft/Perfect-Net.git", from: "3.1.0"),
+		.package(url: "https://github.com/ryancoyne/Perfect-Net.git", from: "4.0.0"),
 		.package(url: "https://github.com/PerfectlySoft/Perfect-Thread.git", from: "3.0.0"),
-		.package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", from: "3.0.12"),
-		.package(url: "https://github.com/PerfectlySoft/Perfect-HTTP.git", from: "3.0.12"),
-		.package(url: "https://github.com/PerfectlySoft/Perfect-Crypto.git", from: "3.0.0")
+		.package(url: "https://github.com/ryancoyne/Perfect-HTTPServer.git", from: "4.0.0"),
+		.package(url: "https://github.com/ryancoyne/Perfect-HTTP.git", from: "4.0.0"),
+		.package(url: "https://github.com/ryancoyne/Perfect-Crypto.git", from: "4.0.0")
 	],
 	targets: [
 		.target(name: "PerfectNotifications", dependencies: ["PerfectHTTPServer", "PerfectNet", "PerfectHTTP", "PerfectThread", "PerfectCrypto"])
